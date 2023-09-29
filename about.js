b5b5aa12f48959a7d5fe68f3bebe58de077ca2a6
Atlas Tran <atlas.tran123@gmail.com>
2023-09-29 16:54:33 -0600

    random restaurant button working

--- about.js
+++ about.js
@@ -18,3 +18,15 @@ form.addEventListener('submit', handleSubmit);
 image.addEventListener('mouseover', compliment)
 
 
+const randomRest = ["https://smashburger.com","https://www.apolloburger.com","https://www.meltyway.com/index.html"]
+
+function randomButton (array){
+	let ran = Math.floor(Math.random()*array.length)
+	return array[ran]
+}
+
+document.getElementById('random').addEventListener('click', () =>{
+	let restaurant = randomButton(randomRest)
+	console.log(restaurant)
+	document.getElementById('test').textContent = restaurant
+})
---
+++ new.js
@@ -0,0 +1,52 @@
+const randomRest = [
+    {
+        link: "https://smashburger.com",
+        name: "SmashBurger"
+    },
+    {
+        link: "https://www.apolloburger.com",
+        name: "Apollo Burger"
+    },
+    {
+        link: "https://www.meltyway.com/index.html",
+        name: "Melty Way"
+    },
+    {
+        link: "http://sabaideethaicuisine.com",
+        name: "Sabaidee Thai Cuisine"
+    },
+    {
+        link: "https://locations.randrbbq.com/ut/lehi/lehi/",
+        name: "R&R BBQ"
+    },
+    {
+        link: "https://taqueria27.com",
+        name: "Taqueria 27"
+    },
+    {
+        link: "https://www.hhc.ooo",
+        name: "Houstons Hot Chicken",
+    },
+    {
+        link: "https://www.slapfishrestaurant.com/restaurant/lehi-ut/ut?utm_source=gmb&utm_medium=yext",
+        name: "SlapFish Coastal Seafood Kitchen"
+    },
+    {
+        link: "https://www.tsunamiutah.com",
+        name: "Tsunami Sushi Bar",
+    }]
+
+    let ran;
+function randomButton (array){
+	ran = Math.floor(Math.random()*array.length)
+	return array[ran]
+}
+
+document.getElementById('random').addEventListener('click', () =>{
+	let restaurant = randomButton(randomRest).link
+    let name = randomButton(randomRest).name
+	console.log(restaurant)
+    console.log(name)
+	document.getElementById('test').setAttribute('href', restaurant)
+    document.getElementById('test').textContent = name
+})
